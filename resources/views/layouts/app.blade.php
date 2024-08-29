b3c1aefacfecdd5b0b7231f0945f8713ac8ee417
thealamdev <shahalam141072@gmail.com>
2024-08-29 12:09:45 +0600

    Dashboard has been added but need to modified

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,41 +1,60 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @livewireStyles
-</head>
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
+    </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
-        @include('layouts.navigation')
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+        @livewireScripts
+        <script>
+            const sidebar = document.getElementById('sidebar');
 
-        <!-- Page Heading -->
-        @isset($header)
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endisset
+            if (sidebar) {
+                const toggleSidebarMobile = (sidebar, sidebarBackdrop, toggleSidebarMobileHamburger, toggleSidebarMobileClose) => {
+                    sidebar.classList.toggle('hidden');
+                    sidebarBackdrop.classList.toggle('hidden');
+                    toggleSidebarMobileHamburger.classList.toggle('hidden');
+                    toggleSidebarMobileClose.classList.toggle('hidden');
+                }
 
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
-    </div>
-    @livewireScripts
-</body>
+                const toggleSidebarMobileEl = document.getElementById('toggleSidebarMobile');
+                const sidebarBackdrop = document.getElementById('sidebarBackdrop');
+                const toggleSidebarMobileHamburger = document.getElementById('toggleSidebarMobileHamburger');
+                const toggleSidebarMobileClose = document.getElementById('toggleSidebarMobileClose');
+                const toggleSidebarMobileSearch = document.getElementById('toggleSidebarMobileSearch');
+
+                toggleSidebarMobileSearch.addEventListener('click', () => {
+                    toggleSidebarMobile(sidebar, sidebarBackdrop, toggleSidebarMobileHamburger, toggleSidebarMobileClose);
+                });
+
+                toggleSidebarMobileEl.addEventListener('click', () => {
+                    toggleSidebarMobile(sidebar, sidebarBackdrop, toggleSidebarMobileHamburger, toggleSidebarMobileClose);
+                });
+
+                sidebarBackdrop.addEventListener('click', () => {
+                    toggleSidebarMobile(sidebar, sidebarBackdrop, toggleSidebarMobileHamburger, toggleSidebarMobileClose);
+                });
+            }
+        </script>
+    </body>
 
 </html>
