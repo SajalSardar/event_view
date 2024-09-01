c66d27d7206631df875b3fc3b6e3f7f12f4547d5
Shah Alam <shahalam141072@gmail.com>
2024-09-01 21:46:43 +0600

    Delete component has been modified- wire:ignore has been added

--- resources/views/components/actions/delete.blade.php
+++ resources/views/components/actions/delete.blade.php
@@ -1,3 +1,3 @@
-<a {!! $attributes->merge(['class' => 'p-2']) !!}>
+<a {!! $attributes->merge(['class' => 'p-2']) !!} wire:ignore>
     <img src="{{ asset('assets/icons/delete.png') }}" alt="__delete">
 </a>
