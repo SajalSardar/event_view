96fa6351295a40988da93190fc5fb84df9f516a6
Shah Alam <shahalam141072@gmail.com>
2024-09-02 17:27:49 +0600

    Some modification in input-icon component

--- resources/views/components/forms/text-input-icon.blade.php
+++ resources/views/components/forms/text-input-icon.blade.php
@@ -7,7 +7,7 @@
 
 <div class="relative">
     <input {!! $attributes->merge(['class' => 'w-full py-3 border border-slate-400 focus:border-primary-400 rounded-lg bg-transparent']) !!} />
-    <span class="absolute inset-y-0 end-0 grid w-10 place-content-center" style="margin-top:-36px;margin-right:10px">
+    <span class="absolute inset-y-0 end-0 grid w-10 place-content-center">
         <button type="button" class="text-gray-600 hover:text-gray-700">
             {{ $slot }}
         </button>
