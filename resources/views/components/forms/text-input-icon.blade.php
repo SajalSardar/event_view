4e303f0369e753f76ca5c701a5c7eef7cf5191e7
Shah Alam <shahalam141072@gmail.com>
2024-09-02 17:29:16 +0600

    Some modification in input-icon component

--- resources/views/components/forms/text-input-icon.blade.php
+++ resources/views/components/forms/text-input-icon.blade.php
@@ -1,10 +1,3 @@
-<!--
-  Heads up! 👋
-
-  Plugins:
-    - @tailwindcss/forms
--->
-
 <div class="relative">
     <input {!! $attributes->merge(['class' => 'w-full py-3 border border-slate-400 focus:border-primary-400 rounded-lg bg-transparent']) !!} />
     <span class="absolute inset-y-0 end-0 grid w-10 place-content-center">
