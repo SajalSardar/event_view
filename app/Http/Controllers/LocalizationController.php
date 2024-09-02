139e00bf9896e7ac75bb09996a28495d8d7bc92a
Shah Alam <shahalam141072@gmail.com>
2024-09-02 18:33:00 +0600

    Localization has been done

---
+++ app/Http/Controllers/LocalizationController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+
+class LocalizationController extends Controller
+{
+    /**
+     * Define public function locale to change language
+     */
+    public function locale($lang)
+    {
+        if (in_array($lang, ['en', 'bn'])) {
+            App::setLocale($lang);
+            Session::put('locale', $lang);
+        }
+        return back();
+    }
+}
