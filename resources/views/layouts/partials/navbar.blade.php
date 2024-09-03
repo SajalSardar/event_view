66aaf7a5c80600fa789a2a3ef003175607a084b0
Shah Alam <shahalam141072@gmail.com>
2024-09-03 22:19:27 +0600

    Dashboard set up final

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -26,10 +26,12 @@
             <div class="flex h-[70px]">
                 <div class="basis-1/3 flex justify-center items-center border border-slate-300 border-r-1 border-l-0 border-t-0 border-b-0">
                     <img src="{{ asset('assets/icons/eng.png') }}" alt="country">
-                    <p class="pl-1">ENG</p>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M6 9L12 15L18 9" stroke="#333333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
+                    <div>
+                        <select onchange="changeLanguage(this.value)" class="border-none bg-transparent">
+                            <option {{ session()->has('locale') && session()->get('locale') == 'en' ? 'selected' : '' }} value="en">ENG</option>
+                            <option {{ session()->has('locale') && session()->get('locale') == 'bn' ? 'selected' : '' }} value="bn">BN</option>
+                        </select>
+                    </div>
                 </div>
                 <div class="basis-1/3 flex justify-center items-center border border-slate-300 border-r-1 border-l-0 border-t-0 border-b-0">
                     <img src="{{ asset('assets/icons/notifications.png') }}" alt="notification" class="pr-3">
@@ -45,6 +47,13 @@
 
         </div>
     </div>
-
-
 </div>
+<script>
+    /**
+     * Define method changeLanguage to change the local language
+     * @param lang 
+     */
+    function changeLanguage(lang) {
+        window.location.href = '{{ url('locale') }}/' + lang;
+    }
+</script>
