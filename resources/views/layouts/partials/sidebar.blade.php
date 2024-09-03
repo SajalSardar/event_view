68de87361b1ae83d3414b2d5b0907f7f827f3cb6
Shah Alam <shahalam141072@gmail.com>
2024-09-03 19:02:16 +0600

    Dashboard navbar has been sligly done

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -3,7 +3,7 @@
         <img src="{{ asset('assets/icons/EventMetro.png') }}" alt="logo">
     </a>
 
-    <ul class="mt-4 bg-primary-500">
+    <ul class="mt-4 bg-primary-500 h-full">
         <li class="mb-1 group flex py-5 px-5">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2 18C2 16.4596 2 15.6893 2.34673 15.1235C2.54074 14.8069 2.80693 14.5407 3.12353 14.3467C3.68934 14 4.45956 14 6 14C7.54044 14 8.31066 14 8.87647 14.3467C9.19307 14.5407 9.45926 14.8069 9.65327 15.1235C10 15.6893 10 16.4596 10 18C10 19.5404 10 20.3107 9.65327 20.8765C9.45926 21.1931 9.19307 21.4593 8.87647 21.6533C8.31066 22 7.54044 22 6 22C4.45956 22 3.68934 22 3.12353 21.6533C2.80693 21.4593 2.54074 21.1931 2.34673 20.8765C2 20.3107 2 19.5404 2 18Z" stroke="#6D4DFF" stroke-width="1.5" />
@@ -53,49 +53,48 @@
             </ul>
         </li>
 
-        <li class="mb-1 group">
-            <a href="" class="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-                <i class='bx bx-list-ul mr-3 text-lg'></i>
-                <span class="text-sm">Activities</span>
-            </a>
-        </li>
-        <span class="text-gray-400 font-bold">BLOG</span>
-        <li class="mb-1 group">
-            <a href="" class="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 sidebar-dropdown-toggle">
-                <i class='bx bxl-blogger mr-3 text-lg'></i>
-                <span class="text-sm">Post</span>
+        <li class="mb-1 group pb-5 px-5">
+            <svg class="inline-block" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M15.5 12C15.5 13.933 13.933 15.5 12 15.5C10.067 15.5 8.5 13.933 8.5 12C8.5 10.067 10.067 8.5 12 8.5C13.933 8.5 15.5 10.067 15.5 12Z" stroke="#6D4DFF" stroke-width="1.5" />
+                <path
+                    d="M21.011 14.0966C21.5329 13.9559 21.7939 13.8855 21.8969 13.7509C22 13.6164 22 13.3999 22 12.967V11.0333C22 10.6004 22 10.3839 21.8969 10.2494C21.7938 10.1148 21.5329 10.0444 21.011 9.90365C19.0606 9.37766 17.8399 7.33858 18.3433 5.40094C18.4817 4.86806 18.5509 4.60163 18.4848 4.44536C18.4187 4.28909 18.2291 4.18141 17.8497 3.96603L16.125 2.9868C15.7528 2.77546 15.5667 2.66979 15.3997 2.69229C15.2326 2.71479 15.0442 2.9028 14.6672 3.2788C13.208 4.73455 10.7936 4.73449 9.33434 3.27871C8.95743 2.9027 8.76898 2.7147 8.60193 2.69219C8.43489 2.66969 8.24877 2.77536 7.87653 2.9867L6.15184 3.96594C5.77253 4.1813 5.58287 4.28898 5.51678 4.44522C5.45068 4.60147 5.51987 4.86794 5.65825 5.40087C6.16137 7.33857 4.93972 9.3777 2.98902 9.90367C2.46712 10.0444 2.20617 10.1148 2.10308 10.2493C2 10.3839 2 10.6004 2 11.0333V12.967C2 13.3999 2 13.6164 2.10308 13.7509C2.20615 13.8855 2.46711 13.9559 2.98902 14.0966C4.9394 14.6226 6.16008 16.6617 5.65672 18.5993C5.51829 19.1322 5.44907 19.3986 5.51516 19.5549C5.58126 19.7112 5.77092 19.8189 6.15025 20.0342L7.87495 21.0135C8.24721 21.2248 8.43334 21.3305 8.6004 21.308C8.76746 21.2855 8.95588 21.0974 9.33271 20.7214C10.7927 19.2645 13.2088 19.2644 14.6689 20.7213C15.0457 21.0974 15.2341 21.2854 15.4012 21.3079C15.5682 21.3304 15.7544 21.2247 16.1266 21.0134L17.8513 20.0341C18.2307 19.8188 18.4204 19.7111 18.4864 19.5548C18.5525 19.3985 18.4833 19.1321 18.3448 18.5992C17.8412 16.6617 19.0609 14.6227 21.011 14.0966Z"
+                    stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" />
+            </svg>
+
+            <a href="#" style="display: inline" class="flex font-semibold items-center text-gray-900 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white sidebar-dropdown-toggle">
+                <i class='bx bx-user mr-3 text-lg'></i>
+                <span class="text-sm">Settings</span>
                 <i class="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
             </a>
-            <ul class="pl-7 mt-2 hidden group-[.selected]:block">
-                <li class="mb-4">
-                    <a href="" class="text-gray-900 text-sm flex items-center hover:text-[#f84525] before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3">All</a>
+            <ul class="pl-6 mt-4 hidden group-[.selected]:block">
+                <li class="mb-4 flex">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.0029 2H10.0062C6.72443 2 5.08355 2 3.92039 2.81382C3.49006 3.1149 3.11577 3.48891 2.81445 3.91891C2 5.08116 2 6.72077 2 10C2 13.2792 2 14.9188 2.81445 16.0811C3.11577 16.5111 3.49006 16.8851 3.92039 17.1862C5.08355 18 6.72443 18 10.0062 18H14.0093C17.2911 18 18.932 18 20.0951 17.1862C20.5254 16.8851 20.8997 16.5111 21.2011 16.0811C21.8156 15.2042 21.9663 14.0941 22 13" stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" />
+                        <path d="M18 9.71428V11M18 9.71428C16.8432 9.71428 15.8241 9.14608 15.2263 8.28331M18 9.71428C19.1568 9.71428 20.1759 9.14608 20.7737 8.28331M15.2263 8.28331L14.0004 9.07143M15.2263 8.28331C14.8728 7.77304 14.6667 7.15973 14.6667 6.5C14.6667 5.84035 14.8727 5.22711 15.2262 4.71688M20.7737 8.28331L21.9996 9.07143M20.7737 8.28331C21.1272 7.77304 21.3333 7.15973 21.3333 6.5C21.3333 5.84035 21.1273 5.22711 20.7738 4.71688M18 3.28571C19.1569 3.28571 20.1761 3.854 20.7738 4.71688M18 3.28571C16.8431 3.28571 15.8239 3.854 15.2262 4.71688M18 3.28571V2M20.7738 4.71688L22 3.92857M15.2262 4.71688L14 3.92857" stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" />
+                        <path d="M11 15H13" stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M12 18V22" stroke="#6D4DFF" stroke-width="1.5" />
+                        <path d="M8 22H16" stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" />
+                    </svg>
+
+
+                    <a href="#" class="pl-3 text-gray-900 text-sm flex items-center hover:text-[#f84525]">Platform Seetings</a>
                 </li>
-                <li class="mb-4">
-                    <a href="" class="text-gray-900 text-sm flex items-center hover:text-[#f84525] before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3">Categories</a>
+                <li class="mb-4 flex">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M20 10C19.9641 7.52043 19.7801 6.11466 18.8365 5.17157C17.6643 4 15.7776 4 12.0043 4C8.23106 4 6.34442 4 5.17221 5.17157C4 6.34315 4 8.22876 4 12C4 15.7712 4 17.6569 5.17221 18.8284C6.23545 19.8911 7.88646 19.9899 11 19.9991" stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M18 19.7143V21M18 19.7143C16.8432 19.7143 15.8241 19.1461 15.2263 18.2833M18 19.7143C19.1568 19.7143 20.1759 19.1461 20.7737 18.2833M15.2263 18.2833L14.0004 19.0714M15.2263 18.2833C14.8728 17.773 14.6667 17.1597 14.6667 16.5C14.6667 15.8403 14.8727 15.2271 15.2262 14.7169M20.7737 18.2833L21.9996 19.0714M20.7737 18.2833C21.1272 17.773 21.3333 17.1597 21.3333 16.5C21.3333 15.8403 21.1273 15.2271 20.7738 14.7169M18 13.2857C19.1569 13.2857 20.1761 13.854 20.7738 14.7169M18 13.2857C16.8431 13.2857 15.8239 13.854 15.2262 14.7169M18 13.2857V12M20.7738 14.7169L22 13.9286M15.2262 14.7169L14 13.9286" stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" />
+                        <path d="M9.5 2V4" stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M14.5 2V4" stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M9.5 20V22" stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M4 9.5H2" stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M4 14.5H2" stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M22 9.5H20" stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M12 8H8" stroke="#6D4DFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+
+                    <a href="" class="pl-3 text-gray-900 text-sm flex items-center hover:text-[#f84525]">System Seetings</a>
                 </li>
             </ul>
-        </li>
-        <li class="mb-1 group">
-            <a href="" class="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-                <i class='bx bx-archive mr-3 text-lg'></i>
-                <span class="text-sm">Archive</span>
-            </a>
-        </li>
-        <span class="text-gray-400 font-bold">PERSONAL</span>
-        <li class="mb-1 group">
-            <a href="" class="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-                <i class='bx bx-bell mr-3 text-lg'></i>
-                <span class="text-sm">Notifications</span>
-                <span class=" md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-red-600 bg-red-200 rounded-full">5</span>
-            </a>
-        </li>
-        <li class="mb-1 group">
-            <a href="" class="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-                <i class='bx bx-envelope mr-3 text-lg'></i>
-                <span class="text-sm">Messages</span>
-                <span class=" md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-green-600 bg-green-200 rounded-full">2
-                    New</span>
-            </a>
         </li>
     </ul>
 </div>
