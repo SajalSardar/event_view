16245e7a5245683b742717434f5bc121c0c35b96
Shah Alam <shahalam141072@gmail.com>
2024-09-04 09:16:51 +0600

    Breadcrumb has been added in separated file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,23 +29,9 @@
         <!-- Navbar End -->
         <div class="pt-8 pl-10">
 
-
-            <!-- Breadcrumb -->
-            <nav class="flex pb-3 text-gray-700 " aria-label="Breadcrumb">
-                <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-                    <li class="inline-flex items-center">
-                        <a href="#" class="inline-flex items-center text-sm font-medium text-gray-400">
-                            DASHBOARD
-                        </a>
-                    </li>
-                    <li>
-                        <div class="flex items-center">
-                            <span class="font-medium text-gray-400">/</span>
-                            <a href="#" class="ms-1 text-sm font-medium text-gray-400">EVENT ORGANIZER DASHBOARD</a>
-                        </div>
-                    </li>
-                </ol>
-            </nav>
+            <!-- Breadcrumb Start -->
+            @include('layouts.partials.breadcrumb')
+            <!-- Breadcrumb End -->
 
             {{ $slot }}
         </div>
