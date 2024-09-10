f54869c2dd25c75b37a13600edc960437a8c329b
thealamdev <shahalam141072@gmail.com>
2024-09-10 21:29:43 +0600

    Table responsive has been done

--- resources/views/adminuser/index.blade.php
+++ resources/views/adminuser/index.blade.php
@@ -1,42 +1,43 @@
 <x-app-layout>
-    <table class="w-full table-fixed">
-        <thead class="w-full bg-slate-100 mb-5">
-            <tr>
-                <th class="text-start ps-10 py-2">User Name</th>
-                <th class="text-start ps-10 py-2">Email</th>
-                <th class="text-start ps-10 py-2">Email Verified</th>
-                <th class="text-start ps-10 py-2">Role</th>
-                <th class="text-start ps-10 py-2">Action</th>
-            </tr>
-        </thead>
-
-        <tbody class="mt-5">
-            @forelse ($collections as $each)
-                <tr class="rounded shadow">
-                    <td class="p-10 flex">
-                        <div class="profile">
-                            <img src="{{ asset('assets/images/user.png') }}" alt="user_picture">
-                        </div>
-                        <div class="infos ps-5">
-                            <h5 class="font-medium text-slate-900">{{ $each?->name }}</h5>
-                        </div>
-                    </td>
-                    <td class="p-10 font-normal text-gray-400">{{ $each?->email }}</td>
-                    <td class="p-10 font-normal text-gray-400">{{ Helper::ISOdate($each?->email_verified_at) }}</td>
-                    <td class="p-10 font-normal text-gray-400">{{ $each?->roles->first()->name }}</td>
-                    <td>
-                        <x-actions.edit route="{{ route('admin.user.edit', ['user' => $each?->id]) }}" />
-                    </td>
-                </tr>
-            @empty
+    <div class="relative overflow-x-auto bg-white">
+        <table class="w-full overflow-x-auto">
+            <thead class="w-full bg-slate-100 mb-5">
                 <tr>
-                    <td colspan="4" class="text-center">
-                        <h5 class="font-medium text-slate-900">No data found !!!</h5>
-                    </td>
+                    <th class="text-start ps-10 py-2">User Name</th>
+                    <th class="text-start ps-10 py-2">Email</th>
+                    <th class="text-start ps-10 py-2">Email Verified</th>
+                    <th class="text-start ps-10 py-2">Role</th>
+                    <th class="text-start ps-10 py-2">Action</th>
                 </tr>
-            @endforelse
+            </thead>
 
-        </tbody>
-    </table>
+            <tbody class="mt-5">
+                @forelse ($collections as $each)
+                    <tr class="rounded shadow">
+                        <td class="p-10 flex">
+                            <div class="profile">
+                                <img src="{{ asset('assets/images/user.png') }}" alt="user_picture">
+                            </div>
+                            <div class="infos ps-5">
+                                <h5 class="font-medium text-slate-900">{{ $each?->name }}</h5>
+                            </div>
+                        </td>
+                        <td class="p-10 font-normal text-gray-400">{{ $each?->email }}</td>
+                        <td class="p-10 font-normal text-gray-400">{{ Helper::ISOdate($each?->email_verified_at) }}</td>
+                        <td class="p-10 font-normal text-gray-400">{{ $each?->roles->first()->name }}</td>
+                        <td>
+                            <x-actions.edit route="{{ route('admin.user.edit', ['user' => $each?->id]) }}" />
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="4" class="text-center">
+                            <h5 class="font-medium text-slate-900">No data found !!!</h5>
+                        </td>
+                    </tr>
+                @endforelse
 
+            </tbody>
+        </table>
+    </div>
 </x-app-layout>
