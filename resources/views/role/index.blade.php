4013200a04b08160bc59ddb3a0b406b8eafb4690
thealamdev <shahalam141072@gmail.com>
2024-09-11 20:40:37 +0600

    Role index page has been modified

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -1,43 +1,49 @@
-<x-app-layout>
+ <x-app-layout>
+     <div class="relative overflow-x-auto bg-white">
+         <div class="flex justify-end pb-3 fixed top-24 right-10">
+             <x-actions.href href="{{ route('dashboard.role.create') }}">
+                 Create Role
+             </x-actions.href>
+         </div>
+         <table class="w-full overflow-x-auto">
+             <thead class="w-full bg-slate-100 mb-5">
+                 <tr>
+                     <th class="text-start ps-10 py-2">#</th>
+                     <th class="text-start ps-10 py-2">Role Name</th>
+                     <th class="text-start ps-10 py-2">Modified At</th>
+                     <th class="text-start ps-10 py-2">Action</th>
+                 </tr>
+             </thead>
 
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+             <tbody class="mt-5">
+                 @forelse ($roles as $each)
+                     <tr class="rounded shadow">
+                         <td class="p-10 flex">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <a href="{{ route('dashboard.role.create') }}">Add Role</a>
+                             <div class="infos ps-5">
+                                 <h5 class="font-medium text-slate-900">{{ $each?->id }}</h5>
+                             </div>
+                         </td>
+                         <td class="p-10 font-normal text-gray-400">{{ $each?->name }}</td>
+                         <td class="p-10 font-normal text-gray-400">
+                             {!! Helper::ISOdate($each->updated_at) !!}
+                         </td>
+                         <td>
+                             <div class="flex">
+                                 <x-actions.edit route="{{ route('dashboard.role.edit', $each->id) }}" />
+                                 {{-- <x-actions.delete action="{{ route('dashboard.role.destroy', ['role' => $each->id]) }}" /> --}}
+                             </div>
+                         </td>
+                     </tr>
+                 @empty
+                     <tr>
+                         <td colspan="4" class="text-center">
+                             <h5 class="font-medium text-slate-900">No data found !!!</h5>
+                         </td>
+                     </tr>
+                 @endforelse
 
-                    <table class="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-                        <thead class="ltr:text-left rtl:text-right">
-                            <tr>
-                                <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-left">Id</th>
-                                <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-left">Role</th>
-                                <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-left">Action</th>
-                            </tr>
-                        <tbody class="divide-y divide-gray-200">
-                            @forelse ($roles as $role)
-                                <tr>
-                                    <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ $role->id }}</td>
-                                    <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ $role->name }}</td>
-                                    <td class="whitespace-nowrap px-4 py-2 text-gray-700">
-                                        <a href="{{ route('dashboard.role.edit', $role->id) }}">edit</a>
-                                    </td>
-                                </tr>
-                            @empty
-                            @endforelse
-
-                        </tbody>
-                        </thead>
-                    </table>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-</x-app-layout>
+             </tbody>
+         </table>
+     </div>
+ </x-app-layout>
