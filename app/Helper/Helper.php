61151174dbfa5490901972cfc5a2eac87f739695
sajalsardar <sajal2.stars@gmail.com>
2024-11-23 18:43:46 +0600

    modify menu

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -2,7 +2,6 @@
 
 use App\Models\Menu;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Session;
 
 class Helper {
@@ -56,20 +55,6 @@ class Helper {
         return Carbon::parse($date)->diffForHumans();
     }
 
-    //get all menu and sub menu
-    public static function getAllMenus() {
-        $menu = Cache::remember('menu_list', 60 * 60, function () {
-            return Menu::with(['submneus' => function ($q) {
-                $q->orderBy('order', 'asc');
-            }])
-                ->where('parent_id', null)
-                ->where('status', 'active')
-                ->orderBy('order', 'asc')
-                ->get();
-        });
-        return $menu;
-    }
-
     //get login user roles
     public static function getLoggedInUserRoles() {
         $user = auth()->user()->load('roles');
@@ -96,4 +81,36 @@ class Helper {
         }
         return false;
     }
+
+    //get all menu and sub menu
+    public static function getAllMenus() {
+        $loginRole = Helper::getLoggedInUserRoleSession();
+        $menus     = Menu::with(['submneus' => function ($q) use ($loginRole) {
+            $q->orderBy('order', 'asc')
+                ->where('status', 'active')
+                ->whereJsonContains('roles', $loginRole);
+        }])
+            ->whereJsonContains('roles', $loginRole)
+            ->where('parent_id', null)
+            ->where('status', 'active')
+            ->orderBy('order', 'asc')
+            ->get();
+
+        return $menus;
+    }
+
+    //get all menu and sub menu
+    // public static function getAllMenus() {
+    //     $menu = Cache::remember('menu_list', 60 * 60, function () {
+    //         return Menu::with(['submneus' => function ($q) {
+    //             $q->orderBy('order', 'asc');
+    //         }])
+    //             ->where('parent_id', null)
+    //             ->where('status', 'active')
+    //             ->orderBy('order', 'asc')
+    //             ->get();
+    //     });
+    //     return $menu;
+    // }
+
 }
